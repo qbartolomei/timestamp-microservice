dd4d03ef55b1e0044e171f8738fae8857480e39e
PhocianTheGood <photiongames@gmail.com>
2019-12-06 17:20:47 -0800

    added solution

--- server.js
+++ server.js
@@ -6,27 +6,33 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/timestamp/:date_string?', function(req, res) {
+	let input = req.params.date_string;
+	if (Number(input)) input = Number(input);
+	let dateTime = new Date(input);
+	if (!input) {
+		dateTime = new Date();
+	}
+	res.json({
+		unix: dateTime.getTime(),
+		utc: dateTime.toUTCString(),
+	});
 });
-
-
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
